b0346df42d3c515f38051d06ce1e3305efdea397
TimothyEniola <timothyyemitan@gmail.com>
2024-06-04 12:40:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let icon=document.querySelector('.icon');
+let ul=document.querySelector('ul');
+
+icon.addEventListener('click', ()=>{
+    ul.classList.toggle('showdata')
+
+    if(ul.className=='showdata'){
+       document.getElementById('bar').className="fa-solid fa-xmark"
+    }else(
+        document.getElementById('bar').className="fa-solid fa-bars"
+    )
+})
+
